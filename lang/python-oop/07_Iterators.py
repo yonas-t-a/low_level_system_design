36d3296fae81cc2f42173a3c36c9604a9d72c124
yonas-t-a <yonas.tessema@aait.edu.et>
2026-09-25 11:49:12 +0300

    List iterator

--- lang/python-oop/07_Iterators.py
+++ lang/python-oop/07_Iterators.py
@@ -13,7 +13,33 @@ class CountTo:
             return value 
         else:
             raise StopIteration
+
+
+class ListIterator:
+    def __init__(self, array):
+        self.array =  array
+        self.index = 0
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.index < len(self.array):
+            value = self.array[self.index]
+            self.index += 1
+            return value
+        else:
+            raise StopIteration
+
+
+
+
 if __name__ == "__main__":
     counter = CountTo(5)
     for i in counter:
         print(i)
+    
+    myList = ListIterator([12,13,14,15,16])
+    for val in myList:
+        print(val)
+        
